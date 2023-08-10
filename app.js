82646a6f151ae4f87550ead94da8b38a9890c038
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-10 17:44:16 -0400

    Adição de novos pacotes, arquivos, funções e etc.

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const connectionDataBase = require('./config/connection');
-const userController = require('./controller/userController');
 const express = require('express');
+const routes = require('./router/routes')
 const app = express();
 const port = 3003;
-app.use(express.json());
+app.use(express.json(), routes);
 app.listen(port, () => { console.log(`Run server...${port}`) });
-
-app.get('/', userController.findUsers);
-
-app.post('/user', userController.createUser);
---
+++ router/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = express.Router();
+
+const userController = require('../controller/userController');
+
+routes.get('/user/:id', userController.findUser);
+
+routes.get('/user', userController.findUsers);
+
+routes.post('/user', userController.createUser);
+
+routes.put('/user/:id', userController.updateUser);
+
+routes.delete('/user/:id', userController.deleteUser);
+
+routes.post('/user/authenticated', userController.authenticateUser);
+
+module.exports = routes;
